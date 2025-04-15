b6b31de8b63d604e0d9aae107f129b696a8e5af8
Kim Robinson <kimmykokonut@gmail.com>
2025-04-15 14:54:12 -0700

    [test] create and pass tests for recipe model

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save recipe without title" do
+    recipe = Recipe.new
+    assert_not recipe.save, "Saved the recipe without a title"
+  end
+
+  test "should have correct associations" do
+    recipe = Recipe.new
+    assert_respond_to recipe, :user
+    assert_respond_to recipe, :tags
+    assert_respond_to recipe, :ingredients
+    assert_respond_to recipe, :recipe_tags
+    assert_respond_to recipe, :ingredient_recipes
+  end
+
+  test "should belong to a user" do
+    recipe = Recipe.new(
+      title: "Test Recipe",
+      instructions: "Test instructions"
+    )
+    assert_not recipe.save, "Saved recipe without a user"
+  end
 end
