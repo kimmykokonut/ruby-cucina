47201886c1f98fb40b89887e78c294bc7a1a0809
Kim Robinson <kimmykokonut@gmail.com>
2025-04-15 00:23:19 +0000

    create recipe and ingredient models and corresponding migration and test files

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_04_15_001440) do
+ActiveRecord::Schema[8.1].define(version: 2025_04_15_002106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "recipe_tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "recipe_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_recipe_tags_on_recipe_id"
+    t.index ["tag_id"], name: "index_recipe_tags_on_tag_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.text "instructions"
+    t.boolean "private"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "tags", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -30,4 +50,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_04_15_001440) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "recipe_tags", "recipes"
+  add_foreign_key "recipe_tags", "tags"
+  add_foreign_key "recipes", "users"
 end
