990113b167a0db39704de027cbb77d3300a7fddc
Kim Robinson <kimmykokonut@gmail.com>
2025-04-21 11:14:55 -0700

    [test-tag] add validations to tag model and pass tests

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save tag without name" do
+    tag = Tag.new
+    assert_not tag.save, "Saved tag without name"
+  end
+
+  test "should not save duplicate tag names" do
+    Tag.create!(name: "Breakfast")
+    tag2 = Tag.new(name: "Breakfast")
+    assert_not tag2.save, "Saved duplicate tag name"
+  end
+
+  test "should have correct associations" do
+    tag = Tag.new
+    assert_respond_to tag, :recipes
+    assert_respond_to tag, :recipe_tags
+  end
+
+  test "can be added to recipes" do
+    tag = tags(:dinner)
+    recipe = recipes(:pasta)
+    tag.recipes << recipe
+    assert tag.recipes.include?(recipe), "Recipe was not added to tag"
+  end
 end
