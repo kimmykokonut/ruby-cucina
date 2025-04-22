ddb41f1b8e546dd5b6ee15888fd9330543e34712
Kim Robinson <kimmykokonut@gmail.com>
2025-04-22 15:12:55 -0700

    [recipe] add basic recipe details page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,11 @@
 class RecipesController < ApplicationController
+  # item list
   def index
     @recipes = Recipe.all
+  end
+
+  # item details
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 end
