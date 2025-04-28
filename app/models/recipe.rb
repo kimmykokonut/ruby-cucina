ee63736db351b28db8d6369ec0b71f142e4d424f
Kim Robinson <kimmykokonut@gmail.com>
2025-04-28 12:58:30 -0700

    add cook time helper methods to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,21 @@ class Recipe < ApplicationRecord
   has_many :ingredient_recipes, dependent: :destroy
   has_many :ingredients, through: :ingredient_recipes
   has_one_attached :photo
+
+  # Helper methods for time calculations
+  def total_time_minutes
+    (prep_time_minutes || 0) + (cook_time_minutes || 0)
+  end
+
+  def time_category
+    total = total_time_minutes
+    case total
+    when 0..60
+      "Under 1 hour"
+    when 61..120
+      "1-2 hours"
+    else
+      "2+ hours"
+    end
+  end
 end
